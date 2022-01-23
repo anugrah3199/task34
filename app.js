b05cbc8745c3e45776226c4fb772d29f1b9217c8
anugrah3199 <anugrahdixit93@gmail.com>
2022-01-23 12:23:36 +0530

    add validation

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const userInput = document.getElementById("username")
-const pswdinput = document.getElementById("password")
+const pswdInput = document.getElementById("pswd")
 const btn = document.querySelector("button")
+
+const regexpswd = "(?=.*?[A-Z])(?=.*?[@])(?=.*?[0-9])"
+const emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+// btn.addEventListener("click", validate)
+
+userInput.addEventListener("change", () => {
+    let inputemail = userInput.value
+    if (inputemail.match(emailregex)) {
+        console.log("match")
+    } else {
+        console.log(" nto macth")
+    }
+})
+pswdInput.addEventListener("change", () => {
+    let pswdval = pswdInput.value
+    if (pswdval.match(regex)) {
+        console.log("match")
+    } else {
+        console.log(" nto macth")
+    }
+})
+
+function validate() {
+    console.log(
+        userInput.value, pswdInput.input
+    )
+}
